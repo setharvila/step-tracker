5769be9959e20707a831f04d8c629c5148d81953
Seth Arvila <setharvila@hotmail.com>
2024-07-09 16:51:01 -0500

    Created XCode Project

---
+++ Step Tracker/Step_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Step_TrackerApp.swift
+//  Step Tracker
+//
+//  Created by Seth Arvila on 7/9/24.
+//
+
+import SwiftUI
+
+@main
+struct Step_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
